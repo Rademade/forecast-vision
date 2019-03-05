e7d767b9d2d4a6297c0142ae8373e6a19e5cde50
Eugene Larychev <larychev93@gmail.com>
2019-03-05 10:55:50 +0200

    fix if no day in week

--- services/forecast/report-member.js
+++ services/forecast/report-member.js
@@ -29,28 +29,16 @@ class ForecastReportMember {
   }
 
   getDaysInMonth(start, end, dayOfWeek) {
-    if (!start && !end) {
+    if (!start || !end) {
       return 1
     }
 
     let days = moment(start).weekdaysInBetween(end, dayOfWeek);
 
-    return days.length
+    return days.length > 0 ? days.length : 1
   }
 
   getAvailableMinutes(startDate, endDate) {
-    console.log(this.getName())
-    // console.log(this.data.monday * this.getDaysInMonth(startDate, endDate, 'Monday'));
-
-    console.log((this.data.monday * this.getDaysInMonth(startDate, endDate, 'Monday')) +
-      (this.data.tuesday * this.getDaysInMonth(startDate, endDate, 'Tuesday')) +
-      (this.data.wednesday * this.getDaysInMonth(startDate, endDate, 'Wednesday')) +
-      (this.data.thursday * this.getDaysInMonth(startDate, endDate, 'Thursday')) +
-      (this.data.friday * this.getDaysInMonth(startDate, endDate, 'Friday')) +
-      (this.data.saturday * this.getDaysInMonth(startDate, endDate, 'Saturday')) +
-      (this.data.sunday * this.getDaysInMonth(startDate, endDate, 'Sunday')))
-
-
     return (this.data.monday * this.getDaysInMonth(startDate, endDate, 'Monday')) +
       (this.data.tuesday * this.getDaysInMonth(startDate, endDate, 'Tuesday')) +
       (this.data.wednesday * this.getDaysInMonth(startDate, endDate, 'Wednesday')) +
